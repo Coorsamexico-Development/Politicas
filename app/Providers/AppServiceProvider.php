2b5118b0261eaa1385377f18c6834cf03590e15b
Hector <programadorjr@coorsamexico.com>
2022-10-13 15:33:38 -0500

    without servicse provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,10 +21,5 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
-    {
-        if(config('app.env') === 'production') {
-            \URL::forceScheme('https');// por google cloud run
-        }
-    }
+   
 }
